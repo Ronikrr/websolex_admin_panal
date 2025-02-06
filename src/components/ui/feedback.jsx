5fe69a42f0b4b250afcfebe4a2ff6945938e4e61
Ronik <ronikgorasiya@gmail.com>
2025-02-06 11:27:22 +0530

    first php

--- src/components/ui/feedback.jsx
+++ src/components/ui/feedback.jsx
@@ -8,22 +8,25 @@ const FeedbackMessage = ({ type, message, onClear }) => {
             setShowMessage(true);
 
             const timer = setTimeout(() => {
-                setShowMessage(false); // Start the fade-out animation
+                setShowMessage(false);
                 setTimeout(() => {
-                    onClear(); // Clear the message after fade-out ends
-                }, 500); // Wait for the fade-out duration before calling onClear
-            }, 5000); // Timeout duration (5 seconds)
+                    onClear();
+                }, 500);
+            }, 5000);
 
-            return () => clearTimeout(timer); // Clean up the timer when the component unmounts
+            return () => clearTimeout(timer);
         }
     }, [message, onClear]);
 
-    if (!message) return null; // Render nothing if there's no message
+    if (!message) return null;
 
     return (
         <div
             className={`fixed top-4 right-0 px-10 py-6 rounded shadow-lg capitalize transition-opacity duration-500 ${showMessage ? "opacity-100 animate-fadeIn" : "opacity-0 animate-fadeOut"
-                } ${type === "success" ? "bg-green-500 text-green-100" : "bg-red-500 text-red-100"}`}
+                } ${type === "success"
+                    ? "bg-green-500 text-green-100"
+                    : "bg-red-500 text-red-100"
+                }`}
         >
             {message}
         </div>
